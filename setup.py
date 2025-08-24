68c96895aeba7684f1809808dd2e12662b57d744
KatakaiAkinori <49186510+akinoria@users.noreply.github.com>
2025-08-24 13:07:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -126,6 +126,8 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
+        cmake_args += ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
+
         subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
 
